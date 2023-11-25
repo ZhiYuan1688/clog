24955d0c1d5aa8257a8bd9c5eda249f7b96d673e
qq24574719 <buzhi518@gmail.com>
2023-11-25 23:38:15 +0800

    修复打包白屏问题

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,40 @@ const routes = [
     {
         path:'/',
         name:"首页",
-        component:()=>import('../views/index.vue')
+        component:()=>import('../views/index.vue'),
+        meta:{
+            title:"首页"
+        }
     },
     {
         path:'/project',
         name:"项目",
-        component:()=>import('../views/project/index.vue')
+        component:()=>import('../views/project/index.vue'),
+        meta:{
+            title:"项目"
+        }
     },
     {
       path:'/hash',
       name:"Hash加密",
-      component:()=>import('../views/crypto/hash.vue')
+      component:()=>import('../views/crypto/hash.vue'),
+      meta:{
+          title:"Hash加密"
+      }
     },
     {
       path:'/aes',
       name:"AES加密",
-      component:()=>import('../views/crypto/aes.vue')
+      component:()=>import('../views/crypto/aes.vue'),
     }
     ,
     {
       path:'/todo',
       name:"Todo",
-      component:()=>import('../views/todo/index.vue')
+      component:()=>import('../views/todo/index.vue'),
+      meta:{
+        title:"Todo"
+      }
     }
    
   ]
--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,7 @@ export default defineConfig(async () => ({
     vue(),
     vitePluginForArco({style: 'css'}),
     UnoCSS(),
-    viteCompression({
-      filter: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i, // 需要压缩的文件
-      threshold: 500, // 文件容量大于这个值进行压缩
-      algorithm: 'gzip', // 压缩方式
-      ext: 'gz', // 后缀名
-      deleteOriginFile: true, // 压缩后是否删除压缩源文件
-    })
+
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
